b6d87fd8127378d79ad21bcb5928df3b913dc9cd
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-08-20 09:26:37 -0400

    fix: improve font sizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 
 const Hero = () => {
-  {
-    /* See https://github.com/mattboldt/typed.js */
-  }
   const el = useRef(null);
   const typed = useRef(null);
 
   useEffect(() => {
+    {
+      /* See https://github.com/mattboldt/typed.js */
+    }
     const options = {
       strings: [
         "Full-Stack Web Developer",
@@ -28,23 +28,23 @@ const Hero = () => {
   }, []);
 
   return (
-    <header className="flex flex-col justify-center items-center text-white min-h-screen p-6 md:p-10">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold text-center mb-4">
+    <header className="flex flex-col justify-center items-center text-white min-h-screen p-4 sm:p-6">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold text-center mb-2 sm:mb-3">
         Hi! I am{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00C299] to-[#0077B6]">
           Rishikesh Kanabar
         </span>
       </h1>
-      <p className="text-base sm:text-sm md:text-md lg:text-lg mb-4 text-center">
-        <span className="font-bold block mb-1">
+      <p className="text-sm sm:text-base md:text-xl lg:text-2xl mb-2 sm:mb-3 text-center">
+        <span className="font-semibold block mb-1">
           <span ref={el}></span>
         </span>
         Crafting Digital Experiences, One Line of Code at a Time
       </p>
-      <nav className="border-t border-gray-600 pt-4 text-center">
+      <nav className="border-t border-gray-600 pt-2 sm:pt-3 text-center">
         <a
           href="#projects"
-          className="text-[#00C299] hover:text-[#c3eaff] font-semibold transition duration-300"
+          className="text-[#00C299] hover:text-[#c3eaff] text-sm sm:text-base md:text-lg lg:text-xl font-semibold transition duration-300"
         >
           Explore My Work
         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,31 @@ const Navbar = () => {
     { label: "Skills", href: "#skills" },
   ];
 
+  {
+    /* TODO: Make the navigation bar sticky */
+  }
   return (
-    <nav className="fixed w-full top-0 z-10 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <a href="#" className="text-[#00C299] text-3xl font-bold" title="Home">
-          Rishi
-        </a>
-        <ul className="flex space-x-8 text-white">
-          {navItems.map((item, index) => (
-            <li key={index} className="hover:text-[#00C299]">
-              <a href={item.href}>{item.label}</a>
-            </li>
-          ))}
-        </ul>
+    <nav className="top-0 z-10 p-4">
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="flex items-center justify-between">
+          <a
+            href="#"
+            className="text-[#00C299] text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold tracking-tight"
+            title="Home"
+          >
+            &lt;Rishi /&gt;
+          </a>
+          <ul className="flex space-x-4 text-white">
+            {navItems.map((item, index) => (
+              <li
+                key={index}
+                className="hover:text-[#00C299] duration-300 text-sm md:text-base lg:text-lg xl:text-xl"
+              >
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
