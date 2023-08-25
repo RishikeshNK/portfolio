e960849f7b905368244a6ba22c5599b6cb5936c5
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-08-25 11:05:11 -0400

    feat: make footer more 'professional'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import { FaHeart } from "react-icons/fa";
-
 import { footer } from "../styles";
 
 const Footer = () => {
   return (
     <footer className={`${footer.footerText} mt-auto text-center`}>
-      Made with <FaHeart className="inline-block text-[#FF6B6B] mb-1" /> by{" "}
+      Made by{" "}
       <a
         className="font-semibold"
         href="https://www.linkedin.com/in/rishikeshnk"
