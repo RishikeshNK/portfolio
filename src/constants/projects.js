56d4f0f4fe8e8dbdc1ddc4785d2dbc79e2a5e5c6
Rishikesh <rishikesh.kanabar@gmail.com>
2023-09-12 12:53:56 -0400

    chore: update simutrade tech stack

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -9,7 +9,7 @@ const projects = [
       "SimuTrade API is a RESTful API that provides a platform for managing and trading simulated stocks and portfolios.",
     live: null,
     code: "https://github.com/RishikeshNK/simutrade-api",
-    technologies: ["ExpressJS", "MongoDB", "NodeJS", "JWT"],
+    technologies: ["ExpressJS", "Docker", "Prisma", "Postgres", "JWT"],
   },
   {
     image: shrinkit,
