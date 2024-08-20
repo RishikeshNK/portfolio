d43166e7623412d0702d6e838cf26b26aab37545
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-08-20 15:47:24 -0400

    feat(content): update resume for aug 2024

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                 </Link>
               </li>
             ))}
-            <li className="nav-item hover-animation font-bold">
+            <li className="nav-item hover-animation">
               <a href={resumeLink}>Resume</a>
             </li>
           </ul>
