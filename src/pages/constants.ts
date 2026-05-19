f0715fe21f0c13c57de689f4411cafee6682b7a5
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2026-05-18 22:15:48 -0400

    chore: update resume link

--- src/pages/constants.ts
+++ src/pages/constants.ts
@@ -2,6 +2,6 @@ export const LINKS = {
     email: "rishi@kanabar.dev",
     github: "https://github.com/RishikeshNK",
     linkedin: "https://linkedin.com/in/rishikeshnk",
-    resume: "https://example.com/resume",
+    resume: "https://drive.google.com/file/d/1gCBbJkiaquo5ALSAKQSDJSv_TRrDJTwI/view?usp=sharing",
     draftkings: "https://www.draftkings.com",
 };
